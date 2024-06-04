203115e8d6b820282c75bc055b5984f1474d22ff
John Chinonso Edeh <netojaycee@gmail.com>
2024-06-04 01:43:37 +0100

    resolved

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -2,11 +2,7 @@ import Logo from "./Logo";
 
 export default function Footer() {
   return (
-<<<<<<< HEAD
     <div className="pt-10 bg-neutral-700 bottom-0 bg-black">
-=======
-    <div className="py-10 bg-neutral-700  ">
->>>>>>> 55fd8696ffc7fca921fdb5f1836b374b74af661e
       <div>
         <div className="text-center text-white md:px-40">
           <div className="md:flex md:justify-center md:space-x-20">
@@ -23,15 +19,12 @@ export default function Footer() {
               <p className="  ">Monday - Friday 10:00 -5:00</p>
               <p className=" ">Saturday - by apppointment</p>
             </div>
-<<<<<<< HEAD
             <div>
               <h2 className="font-bold text-xl mt-8">ONLINE SHOP</h2>
               <p className="  ">
                 Now you can place ordr from our showroom online.
               </p>
             </div>
-=======
->>>>>>> 55fd8696ffc7fca921fdb5f1836b374b74af661e
           </div>
         </div>
       </div>
